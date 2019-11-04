5bab292c08cb4bd3b696a318b795f23ed27400d6
Aaron Lee <leeaaron326@gmail.com>
2019-11-03 23:35:39 -0500

    Added dynamic text field to be used in List Record

---
+++ lib/components/DynamicTextField.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class DynamicTextField extends StatefulWidget {
+
+  final String value;
+
+  DynamicTextField({this.value});
+
+  @override
+  _DynamicTextFieldState createState() {
+    return _DynamicTextFieldState(this.value);
+  }
+}
+
+class _DynamicTextFieldState extends State<DynamicTextField> {
+
+  String value;
+  Widget valueWidget;
+
+  _DynamicTextFieldState(var value) {
+    this.value = value;
+    this.valueWidget = _valueDisplay(value);
+  }
+
+  Widget _valueDisplay(val) {
+    return new GestureDetector(
+      child: Text(val),
+      onLongPress: () {
+        setState(() {
+          this.valueWidget = _valueEdit(val);
+        });
+      },
+    );
+  }
+
+  Widget _valueEdit(value) {
+    return TextFormField(
+      initialValue: value,
+      onFieldSubmitted: (val) {
+        setState(() {
+          this.value = val;
+          this.valueWidget = _valueDisplay(val);
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return valueWidget;
+  }
+
+}
