01a794b72cad848f29f1566915bddd472086e5d3
Aaron Lee <leeaaron326@gmail.com>
2019-12-17 00:10:49 -0500

    Added dummy data for calendar overview

--- test/data_template.dart
+++ test/data_template.dart
@@ -2,8 +2,10 @@ import 'package:tokyo_mobile/models/exercise_record.dart';
 import 'package:uuid/uuid.dart';
 
 class DataTemplate {
+  Uuid uuid = Uuid();
 
-  ExerciseRecord exerciseRecord({String exRecId, String workoutId, DateTime createdDate}) {
+  ExerciseRecord exerciseRecord(
+      {String exRecId, String workoutId, DateTime createdDate}) {
     ExerciseRecord exerciseRecord = defaultExerciseRecord();
     exerciseRecord.exerciseRecId = exRecId;
     exerciseRecord.workoutId = workoutId;
@@ -13,16 +15,60 @@ class DataTemplate {
 
   ExerciseRecord defaultExerciseRecord() {
     return ExerciseRecord.value(
-      workoutId: Uuid().v4(),
-      exerciseRecId: "1",
-      exerciseName: "Squat",
-      exerciseSets: [],
-      createdDate: DateTime.now(),
-      completedDate: DateTime.now()
-    );
+        workoutId: uuid.v4(),
+        exerciseRecId: uuid.v4(),
+        exerciseName: "Squat",
+        exerciseSets: [],
+        createdDate: DateTime.now(),
+        completedDate: DateTime.now());
   }
 
+  List<ExerciseRecord> monthOfExerciseRecords() {
+    // Regular Case
+    String clipsStartOfMonth = uuid.v4();
+    String regularWorkout = uuid.v4();
+    String clipsEndOfMonth = uuid.v4();
 
+    List<ExerciseRecord> start = [
+      DateTime.utc(2020, 10, 31, 23, 58, 30),
+      DateTime.utc(2020, 11, 01, 00, 00, 00),
+      DateTime.utc(2020, 11, 01, 00, 02, 30)
+    ].map((date) {
+      exerciseRecord(
+          exRecId: uuid.v4(), workoutId: clipsStartOfMonth, createdDate: date);
+    }).toList();
 
+    List<ExerciseRecord> regularOne = [
+      DateTime.utc(2020, 11, 15, 11, 54, 30),
+      DateTime.utc(2020, 11, 15, 11, 58, 30),
+      DateTime.utc(2020, 11, 15, 12, 00, 00),
+      DateTime.utc(2020, 11, 15, 12, 03, 15),
+      DateTime.utc(2020, 11, 15, 12, 07, 30)
+    ].map((date) {
+      exerciseRecord(
+          exRecId: uuid.v4(), workoutId: regularWorkout, createdDate: date);
+    }).toList();
 
+    List<ExerciseRecord> regularTwo = [
+      DateTime.utc(2020, 11, 17, 11, 54, 30),
+      DateTime.utc(2020, 11, 17, 11, 58, 30),
+      DateTime.utc(2020, 11, 17, 12, 00, 00),
+      DateTime.utc(2020, 11, 17, 12, 03, 15),
+      DateTime.utc(2020, 11, 17, 12, 07, 30)
+    ].map((date) {
+      exerciseRecord(
+          exRecId: uuid.v4(), workoutId: regularWorkout, createdDate: date);
+    }).toList();
+
+    List<ExerciseRecord> end = [
+      DateTime.utc(2020, 11, 30, 23, 58, 30),
+      DateTime.utc(2020, 12, 01, 00, 00, 00),
+      DateTime.utc(2020, 12, 01, 00, 02, 30)
+    ].map((date) {
+      exerciseRecord(
+          exRecId: uuid.v4(), workoutId: clipsEndOfMonth, createdDate: date);
+    }).toList();
+
+    return start + regularOne + regularTwo + end;
+  }
 }
